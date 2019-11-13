84c4fa39cb400087a4dca3101bcb93f23c975734
David Li <dawei.li@gmx.net>
2019-11-13 20:08:07 +0100

    using mathquill from fork-repo

--- src/input/mathquill-loader.js
+++ src/input/mathquill-loader.js
@@ -1,9 +1,16 @@
 // this file needs to be .js to have access to webpack loaders
 
-import MathQuill from 'exports-loader?window.MathQuill!imports-loader?window.jQuery=jquery/dist/jquery.slim!../../external/mathquill.js'
+import MathQuill from 'exports-loader?window.MathQuill!imports-loader?window.jQuery=jquery/dist/jquery.slim!@entkenntnis/mathquill/build/mathquill.js'
+
+import '@entkenntnis/mathquill/build/mathquill.css'
+
+/*
+Useful for local development:
+
+import MathQuill from 'exports-loader?window.MathQuill!imports-loader?window.jQuery=jquery/dist/jquery.slim!../../.external/mathquill.js'
+
+import '../../.external/mathquill.css'
+*/
 
 MathQuill.getInterface(2)
-
-import '../../external/mathquill.css'
-
 export default MathQuill
